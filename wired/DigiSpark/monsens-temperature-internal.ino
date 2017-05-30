c9e61e5a1b5e54bad519a47d775b97491304c14f
Simon Rupf <simon@rupf.net>
2017-05-30 22:31:30 +0200

    prototype, carriage return and menu not yet working

--- wired/DigiSpark/monsens-temperature-internal.ino
+++ wired/DigiSpark/monsens-temperature-internal.ino
@@ -7,17 +7,30 @@
 
 #include <DigiCDC.h>
 
-#define prompt "> "
-
-String input;
+const char prompt[] = "> ";
 
 /**
  * initialize USB interface
  */
-void setup() {                
-  SerialUSB.begin(); 
-  SerialUSB.write(prompt);
+void setup() {
+  SerialUSB.begin();
+  pinMode(1, OUTPUT); // LED on Model A
   analogReference(INTERNAL1V1);
+  SerialUSB.refresh();
+  SerialUSB.print(prompt);
+}
+
+/**
+ * print characters and return cursor
+ */
+void println(char output[]) {
+  //SerialUSB.refresh();
+  SerialUSB.print(output);
+  //SerialUSB.refresh();
+  //SerialUSB.print("\r");
+  //SerialUSB.refresh();
+  SerialUSB.print("\n");
+  //SerialUSB.refresh();
 }
 
 /**
@@ -27,33 +40,29 @@ void setup() {
  * then evaluate it and print an according result or usage instructions
  */
 void loop() {
-  while (SerialUSB.available()) {
-    SerialUSB.delay(3);             // delay to allow buffer to fill
-    if (SerialUSB.available() > 0) {
-      char c = SerialUSB.read();    // gets one byte from serial buffer
-      if (c == F("\n")) {
-        break;
-      }
-      input += c;
-    }
-  }
-
-  if (input.length() > 0) {
-    int temp = analogRead(A0+15);
-    if (input == F("C") || input == F("K")) {
-      if (input == F("C")) {
+  if (SerialUSB.available()) {
+    char c[1];
+    c[0] = SerialUSB.read();  // gets one byte from serial buffer
+    digitalWrite(1, HIGH);    // turn the LED on (HIGH is the voltage level)
+    println(c);
+    if (c[0] == 'C' || c[0] == 'K') {
+      int temp = analogRead(A0+15);
+      SerialUSB.refresh();
+      if (c[0] == 'C') {
         temp -= 273;
       }
-      SerialUSB.println(temp);
-    } else {
-      SerialUSB.println(F("Returns temperature as Celsius or Kelvin. List of commands:"));
-      SerialUSB.println(F("C"));
-      SerialUSB.println(F("K"));
-    }
-    SerialUSB.write(prompt);
-    input = "";
+      char val[5];
+      sprintf(val, "%d", temp + 8);
+      println(val);
+/*    } else {
+      println("commands:");
+      println("C");
+      println("K");
+*/    }
+    SerialUSB.delay(50);
+    digitalWrite(1, LOW);     // turn the LED off by making the voltage LOW
+    SerialUSB.print(prompt);
   }
-
-  SerialUSB.delay(100);
+  SerialUSB.delay(3);
 }
 
