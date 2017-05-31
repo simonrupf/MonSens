c2001bb6be83ffcfc1e628c358be9539d06017a1
Simon Rupf <simon@rupf.net>
2017-05-31 22:11:20 +0200

    working prototype using the internal temperature sensor of the DigiSpark

--- wired/DigiSpark/README.md
+++ wired/DigiSpark/README.md
@@ -39,4 +39,47 @@ sketchbook folder:
 $ ./initialize-sketchbooks.sh ~/Arduino
 ```
 
-You should
+You should now find a sketchbook called "monsens-temperature-internal" in the
+Arduino IDE and can compile it. The Arduino IDE will tell you when you have to
+plug in the DigiSpark into any USB port for the upload to start.
+
+Each time you plugin the DigiSpark, a serial interface will appear after about
+5 seconds, the time window for programming the device. Check the output of dmesg
+to find the name of the device on your system; Usually it will be called
+/dev/ttyACM0.
+
+To make the measurments accessible via telnet (as may be used by Zabbix) you
+could use netcat for a one-shot or xinetd or socat for a permanent solution.
+
+```bash
+echo -n C > /dev/ttyACM0; head -n1 /dev/ttyACM0 | nc -l 4000
+```
+
+For xinetd create a file with the following content in the /etc/xinetd.d folder
+and restart the service. If possible use a user from the dialup group instead of
+root. The user just needs access to the USB device.
+
+```xinetd
+service testservice
+{
+    port            = 4000
+    socket_type     = stream
+    protocol        = tcp
+    wait            = no
+    user            = root
+    server          = /bin/sh
+    server_args     = -c "echo -n C > /dev/ttyACM0; head -n1 /dev/ttyACM0"
+} 
+```
+
+And the socat variant:
+
+```bash
+socat tcp-l:4000,reuseaddr,fork system:"echo -n C > /dev/ttyACM0; head -n1 /dev/ttyACM0"
+```
+
+In all these example the TCP port 4000 is used, which you may change as you
+like. If you would prefer the measurement in Kelvin instead of Celsius, change
+the "echo -n C" bit to "echo -n K".
+
+
--- wired/DigiSpark/monsens-temperature-internal.ino
+++ wired/DigiSpark/monsens-temperature-internal.ino
@@ -7,61 +7,36 @@
 
 #include <DigiCDC.h>
 
-const char prompt[] = "> ";
-
 /**
  * initialize USB interface
  */
 void setup() {
   SerialUSB.begin();
-  pinMode(1, OUTPUT); // LED on Model A
   analogReference(INTERNAL1V1);
-  SerialUSB.refresh();
-  SerialUSB.print(prompt);
-}
-
-/**
- * print characters and return cursor
- */
-void println(char output[]) {
-  //SerialUSB.refresh();
-  SerialUSB.print(output);
-  //SerialUSB.refresh();
-  //SerialUSB.print("\r");
-  //SerialUSB.refresh();
-  SerialUSB.print("\n");
-  //SerialUSB.refresh();
 }
 
 /**
  * main loop
  *
- * read characters off the serial interface until a line break is received,
- * then evaluate it and print an according result or usage instructions
+ * read characters off the serial interface, then evaluate it and print an
+ * according result
  */
 void loop() {
   if (SerialUSB.available()) {
-    char c[1];
-    c[0] = SerialUSB.read();  // gets one byte from serial buffer
-    digitalWrite(1, HIGH);    // turn the LED on (HIGH is the voltage level)
-    println(c);
-    if (c[0] == 'C' || c[0] == 'K') {
+    char c;
+    c = SerialUSB.read(); // gets one byte from serial buffer
+    SerialUSB.delay(50);  // wait for the shell to be ready to read
+    if (c == 'C' || c == 'K') {
       int temp = analogRead(A0+15);
       SerialUSB.refresh();
-      if (c[0] == 'C') {
+      if (c == 'C') {
         temp -= 273;
       }
       char val[5];
       sprintf(val, "%d", temp + 8);
-      println(val);
-/*    } else {
-      println("commands:");
-      println("C");
-      println("K");
-*/    }
-    SerialUSB.delay(50);
-    digitalWrite(1, LOW);     // turn the LED off by making the voltage LOW
-    SerialUSB.print(prompt);
+      SerialUSB.print(val);
+      SerialUSB.print("\n");
+    }
   }
   SerialUSB.delay(3);
 }
